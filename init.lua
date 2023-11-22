b08d72d860818a0c0a2c6fb2fe5f9566383a90c1
TardyPurcell <tardypurcell@outlook.com>
2023-11-22 23:30:01 +0800

    ...

--- init.lua
+++ init.lua
@@ -183,7 +183,11 @@ require('lazy').setup({
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help ibl`
     main = 'ibl',
-    opts = {},
+    opts = {
+      indent = {
+        char = '┊',
+      },
+    },
   },
 
   -- "gc" to comment visual regions/lines
@@ -222,8 +226,8 @@ require('lazy').setup({
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
-  -- require 'kickstart.plugins.autoformat',
-  -- require 'kickstart.plugins.debug',
+  require 'kickstart.plugins.autoformat',
+  require 'kickstart.plugins.debug',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    You can use this folder to prevent any conflicts with this init.lua if you're interested in keeping
@@ -231,7 +235,7 @@ require('lazy').setup({
   --    Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --
   --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
 }, {})
 
 -- [[ Setting options ]]
@@ -278,6 +282,7 @@ vim.o.termguicolors = true
 -- Mine
 vim.o.cursorline = true;
 vim.o.relativenumber = true;
+vim.o.shiftwidth = 2;
 
 -- [[ Basic Keymaps ]]
 
