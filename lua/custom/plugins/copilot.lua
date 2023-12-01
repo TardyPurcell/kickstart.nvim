92e4d6957508650de8fef484097ae6619a0de86e
TardyPurcell <tardypurcell@outlook.com>
2023-12-02 01:36:19 +0800

    substitute `copilot.nvim` for `copilot.lua`.

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,4 +1,15 @@
 return {
-  'github/copilot.vim',
+  'zbirenbaum/copilot.lua',
   name = 'copilot',
+  config = function()
+    require('copilot').setup({
+      suggestion = {
+        auto_trigger = true,
+        keymap = {
+          accept = '<C-j>'
+        }
+      },
+    })
+    vim.cmd 'Copilot disable'
+  end
 }
